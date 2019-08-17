4331c4d03d80bddf7a985585c9387f4686011bc7
intrening <wol87@ya.ru>
2019-08-17 19:57:50 +0300

    Add Complaint model

--- property/admin.py
+++ property/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 
-from .models import Flat
+from .models import Flat, Complaint
 
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ['town']
-    readonly_fields = ['created_at']
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
-    list_editable = ['new_building']
-    list_filter = ['new_building', 'rooms_number', 'has_balcony']
+    search_fields = ('town',)
+    readonly_fields = ('created_at',)
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town',)
+    list_editable = ('new_building',)
+    list_filter = ('new_building', 'rooms_number', 'has_balcony',)
+
+class CompliantAdmin(admin.ModelAdmin):
+    raw_id_fields = ('flat',)
 
 admin.site.register(Flat, FlatAdmin)
+admin.site.register(Complaint, CompliantAdmin)
